98e1bafa01388ac063aba6fe0cdc46486a3aa36f
MaxwellJChen <maxwelljchen1@gmail.com>
2025-06-19 08:02:08 -0700

    initial commit

---
+++ build/CMakeFiles/LearnOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnOpenGL.dir/src/main.c.o"
+  "CMakeFiles/LearnOpenGL.dir/src/main.c.o.d"
+  "bin/LearnOpenGL"
+  "bin/LearnOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LearnOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
